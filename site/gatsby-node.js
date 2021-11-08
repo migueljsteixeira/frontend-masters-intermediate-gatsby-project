4c000adc76c1295fdeedcecf65d561dc1667a6dd
Miguel Teixeira <64308962+migueljsteixeira@users.noreply.github.com>
2021-11-08 22:09:01 +0000

    Adding custom data

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,3 +1,24 @@
+const authors = require('./src/data/authors.json');
+const books = require('./src/data/books.json');
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const { createNode } = actions;
+
+  authors.forEach((author) => {
+    createNode({
+      ...author,
+      id: createNodeId(`author${author.slug}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'author',
+        content: JSON.stringify(author),
+        contentDigest: createContentDigest(author),
+      },
+    });
+  });
+};
+
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
 
